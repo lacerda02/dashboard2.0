119864622c828e481f732e2b08dc1e4344f9f116
Lacerda02 <113895226+lacerda02@users.noreply.github.com>
2023-03-22 17:39:53 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer class="boyah">
+        <div><a href="home.php">CoreUI </a><a href="home.php">Bootstrap Admin Template</a> © 2022 creativeLabs.</div>
+        <div class="ms-auto">Powered by&nbsp;<a href="https://coreui.io/docs/">CoreUI UI Components</a></div>
+      </footer>
